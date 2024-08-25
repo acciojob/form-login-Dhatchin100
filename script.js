484a4ddf57e3afd2e32562a302c328adeb3c7de1
Dhatchin100 <dhatchin100@gmail.com>
2024-08-25 10:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function getFormvalue() {
-    //Write your code here
-let first = document.getElementById("first").value;
-	let last = document.getElementById("last").value;
-	alert(first+" "+last);
+function displayFormValues(){ 
+	var name=document.getElementById('fname').value;
+	var last=document.getElementById('lname').value;
+	var phone=document.getElementById('Pnum').value;
+	var email=document.getElementById('eid').value;
+
+	alert('First Name:'+name+'\nLast Name:'+last+'\nPhone Number:'+phone+'\nEmail ID:'+email);
 }
