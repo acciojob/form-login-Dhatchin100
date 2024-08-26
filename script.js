b6880ed814f645e7cbeadd5a02c042be5e266f7f
Dhatchin100 <dhatchin100@gmail.com>
2024-08-26 22:19:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function displayFormValues(){ 
-	var name=document.getElementById('fname').value;
-	var last=document.getElementById('lname').value;
-	var phone=document.getElementById('Pnum').value;
-	var email=document.getElementById('eid').value;
+// script.js
+function getFormValue() {
+    // Get the form element
+    const form = document.getElementById("myForm");
 
-	alert('First Name:'+name+'\nLast Name:'+last+'\nPhone Number:'+phone+'\nEmail ID:'+email);
+    // Get the values of the first name and last name fields
+    const firstName = form.elements.fname.value;
+    const lastName = form.elements.lname.value;
+
+    // Display the full name using an alert
+    alert(firstName + " " + lastName);
 }
